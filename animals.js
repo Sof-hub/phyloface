4c68935d26c76ff537b42c23c28096d1e095343b
Sof-hub <sofia.quinteros@hotmail.com>
2026-05-10 19:11:48 +0200

    Change animal images from .jpg to .png format

--- animals.js
+++ animals.js
@@ -2,35 +2,35 @@ const animals = [
 
 {
   name: "Aye-Aye",
-  image: "assets/animals/ayeaye.jpg",
+  image: "assets/animals/ayeaye.png",
   description:
     "Chaotic nocturnal researcher with snack-stealing behavior.",
 },
 
 {
   name: "Blobfish",
-  image: "assets/animals/blobfish.jpg",
+  image: "assets/animals/blobfish.png",
   description:
     "Emotionally exhausted deep-sea conference survivor.",
 },
 
 {
   name: "Shoebill",
-  image: "assets/animals/shoebill.jpg",
+  image: "assets/animals/shoebill.png",
   description:
     "Intimidates everyone without saying a word.",
 },
 
 {
   name: "Axolotl",
-  image: "assets/animals/axolotl.jpg",
+  image: "assets/animals/axolotl.png",
   description:
     "Permanent student form. Never fully evolves.",
 },
 
 {
   name: "Tarsier",
-  image: "assets/animals/tarsier.jpg",
+  image: "assets/animals/tarsier.png",
   description:
     "Sleep-deprived caffeine organism with giant eyes.",
 },
@@ -44,14 +44,14 @@ const animals = [
 
 {
   name: "Saiga",
-  image: "assets/animals/saiga.jpg",
+  image: "assets/animals/saiga.png",
   description:
     "Aerodynamically optimized weird ungulate.",
 },
 
 {
   name: "Dumbo Octopus",
-  image: "assets/animals/dumbo.jpg",
+  image: "assets/animals/dumbo.png",
   description:
     "Cute abyssal creature avoiding social interaction.",
 }
