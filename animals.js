f7051af3470dd3abbddc476762f38a0979d21a0b
Sof-hub <sofia.quinteros@hotmail.com>
2026-05-16 14:05:12 +0200

    Update animals.js

--- animals.js
+++ animals.js
@@ -54,6 +54,196 @@ const animals = [
   image: "assets/animals/dumbo.png",
   description:
     "Cute abyssal creature avoiding social interaction.",
+},
+
+{
+  name: "Proboscis Monkey",
+  image: "assets/animals/proboscis-monkey.png",
+  description:
+    "Loud tropical researcher powered entirely by chaos and potassium.",
+},
+
+{
+  name: "Star-Nosed Mole",
+
+  image: "assets/animals/star-nosed-mole.png",
+
+  description:
+    "Sensory overload specialist with forbidden spaghetti face.",
+
+  traits: {
+    eyeRatio: 0.18,
+    noseWidth: 0.28,
+    faceLength: 0.85
+  }
+},
+
+{
+  name: "Goblin Shark",
+
+  image: "assets/animals/goblin-shark.png",
+
+  description:
+    "Deep-sea cryptid that should not exist but somehow got tenure.",
+
+  traits: {
+    eyeRatio: 0.16,
+    noseWidth: 0.42,
+    faceLength: 1.60
+  }
+},
+
+{
+  name: "Kakapo",
+
+  image: "assets/animals/kakapo.png",
+
+  description:
+    "Flightless emotional support avocado.",
+
+  traits: {
+    eyeRatio: 0.30,
+    noseWidth: 0.20,
+    faceLength: 1.10
+  }
+},
+
+{
+  name: "Mantis Shrimp",
+
+  image: "assets/animals/mantis-shrimp.png",
+
+  description:
+    "Sees colors beyond human comprehension and still chooses violence.",
+
+  traits: {
+    eyeRatio: 0.28,
+    noseWidth: 0.25,
+    faceLength: 1.00
+  }
+},
+
+{
+  name: "Leafy Seadragon",
+
+  image: "assets/animals/leafy-seadragon.png",
+
+  description:
+    "Aquatic leaf pretending to have a PhD.",
+
+  traits: {
+    eyeRatio: 0.26,
+    noseWidth: 0.22,
+    faceLength: 1.20
+  }
+},
+
+{
+  name: "Frilled Shark",
+
+  image: "assets/animals/frilled-shark.png",
+
+  description:
+    "Living fossil fueled by existential dread.",
+
+  traits: {
+    eyeRatio: 0.16,
+    noseWidth: 0.35,
+    faceLength: 1.70
+  }
+},
+
+{
+  name: "Hoatzin",
+
+  image: "assets/animals/hoatzin.png",
+
+  description:
+    "Smells terrible yet remains deeply confident.",
+
+  traits: {
+    eyeRatio: 0.26,
+    noseWidth: 0.30,
+    faceLength: 1.30
+  }
+},
+
+{
+  name: "Pangolin",
+
+  image: "assets/animals/pangolin.png",
+
+  description:
+    "Anxiety pinecone with strong introvert adaptations.",
+
+  traits: {
+    eyeRatio: 0.22,
+    noseWidth: 0.18,
+    faceLength: 1.00
+  }
+},
+
+{
+  name: "Yeti Crab",
+
+  image: "assets/animals/yeti-crab.png",
+
+  description:
+    "Hydrothermal vent cryptid with luxurious arm hair.",
+
+  traits: {
+    eyeRatio: 0.18,
+    noseWidth: 0.30,
+    faceLength: 0.90
+  }
+},
+
+{
+  name: "Glass Frog",
+
+  image: "assets/animals/glass-frog.png",
+
+  description:
+    "Transparent. Fragile. 100% doing its best.",
+
+  traits: {
+    eyeRatio: 0.24,
+    noseWidth: 0.20,
+    faceLength: 0.80
+  }
+},
+
+{
+  name: "UNCLASSIFIED ENTITY",
+
+  image: "assets/animals/unclassified-entity.png",
+
+  description:
+    "Evolution refuses responsibility.",
+
+  rarity: 0.005
+},
+
+{
+  name: "BIBLICALLY ACCURATE RESEARCHER",
+
+  image: "assets/animals/biblically-accurate.png",
+
+  description:
+    "Too many eyes detected.",
+
+  rarity: 0.002
+},
+
+{
+  name: "REVIEWER #2",
+
+  image: "assets/animals/reviewer2.png",
+
+  description:
+    "Hostile phenotype identified.",
+
+  rarity: 0.001
 }
 
 ];
